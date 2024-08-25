f761a9f1bf1ad390ec2935f5fe953bfbff60f45c
vPEa27 <virin.aleksiys@gmail.com>
2024-08-25 03:34:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 // Инициализация Web App
 const tg = window.Telegram.WebApp;
 
-tg.expand(); // Развернуть Web App на весь экран
+// Список ссылок на изображения
+const images = [
+    "https://avatars.mds.yandex.net/i?id=c2bc7833f798c98dc32347e7e1f8da46ceffd2e1-9196574-images-thumbs&n=13",
+    "https://avatars.mds.yandex.net/i?id=2ae9b3fa60168135c60ea277e7a2aa179c57c769-9181437-images-thumbs&n=13",
+    "https://avatars.mds.yandex.net/i?id=e4ee40bba2d4dbc0448410365a744d3006d9deab723354e8-4591292-images-thumbs&n=13"
+];
 
-// Вывести приветственное сообщение
-document.getElementById('greeting').innerText = `Привет, ${tg.initDataUnsafe.user.first_name}!`;
+let currentIndex = 0;
 
-// Обработчик для изменения текста по кнопке
-document.getElementById('changeText').addEventListener('click', () => {
-    document.getElementById('greeting').innerText = 'Вы изменили текст!';
+// Функция обновления изображения
+function updateImage() {
+    const photoElement = document.getElementById('photo');
+    photoElement.src = images[currentIndex];
+}
+
+// Обработчики кнопок
+document.getElementById('prevButton').addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+        updateImage();
+    }
+});
+
+document.getElementById('nextButton').addEventListener('click', () => {
+    if (currentIndex < images.length - 1) {
+        currentIndex++;
+        updateImage();
+    }
 });
 
 // Логирование закрытия Web App
